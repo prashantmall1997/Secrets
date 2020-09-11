ddb7f54abc59da8c6c91b0dbb420ee3cdc4d66e9
prashantmall1997 <prashantmall1997@gmail.com>
2020-09-11 13:32:28 +0530

    Level 2 Security - Add Environment Variables

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,6 +1,8 @@
 //jshint esversion:8
 
+require('dotenv').config();
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 const Schema = mongoose.Schema;
 mongoose.set('useCreateIndex', true);
 
@@ -14,6 +16,12 @@ const userSchema = new Schema({
     required: true,
     type: String
   }
+});
+
+//Mongoose Secrert
+userSchema.plugin(encrypt, {
+  secret: process.env.SECRET,
+  encryptedFields: ['password']
 });
 
 const userModel = mongoose.model('user', userSchema);
